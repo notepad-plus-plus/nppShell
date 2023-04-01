13208989c0a434dff0c1aa0accffdb3f821723a7
Don Ho <don.h@free.fr>
2023-04-01 02:04:29 +0200

    Remove added blank lines

--- Installer.cpp
+++ Installer.cpp
@@ -351,14 +351,12 @@ HRESULT NppShell::Installer::Install()
     UnregisterOldContextMenu();
 
     // Ensure we have removed any old files that might be left behind.
-
     MoveFileToTempAndScheduleDeletion(GetApplicationPath() + L"\\NppShell_01.dll");
     MoveFileToTempAndScheduleDeletion(GetApplicationPath() + L"\\NppShell_02.dll");
     MoveFileToTempAndScheduleDeletion(GetApplicationPath() + L"\\NppShell_03.dll");
     MoveFileToTempAndScheduleDeletion(GetApplicationPath() + L"\\NppShell_04.dll");
     MoveFileToTempAndScheduleDeletion(GetApplicationPath() + L"\\NppShell_05.dll");
     MoveFileToTempAndScheduleDeletion(GetApplicationPath() + L"\\NppShell_06.dll");
-
 
     // Since we have unregistered the old context menu, we refresh the shell, just to be sure.
     SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_IDLIST, 0, 0);
