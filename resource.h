fc47a3b3a8f7790fafc4e96ed36f9768f0747ea7
Don Ho <don.h@free.fr>
2023-08-03 19:14:12 +0200

    NppShell v1.2 release

--- resource.h
+++ resource.h
@@ -4,8 +4,8 @@
 //
 #define IDS_EDIT_WITH_NOTEPADPLUSPLUS   101
 
-#define VERSION_VALUE "1.1"
-#define VERSION_DIGITALVALUE 1,1,0,0
+#define VERSION_VALUE "1.2"
+#define VERSION_DIGITALVALUE 1,2,0,0
 
 // Next default values for new objects
 // 
